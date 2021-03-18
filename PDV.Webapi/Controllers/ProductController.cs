e9891f7f537e27853c89a2cb2d02de27e3b7bfb9
Vinicius Santana <vinicius.santana@localiza.com>
2021-03-18 19:37:21 -0300

    First commit

---
+++ PDV.Webapi/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PDV.Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IRestClient http;
+        public ProductController(IRestClient http)
+        {
+            this.http = http;
+            this.http.BaseUrl = new Uri("https://localhost:44382/");
+        }
+
+        //HTTP -> POST, PUT, DELETE, GET
+        //Request - Response
+        //Códigos
+        //200 - OK
+        //404 - Bad Request
+        //500 - internal server error
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var uri = "api/Inventory";
+
+            var request = new RestRequest(uri, Method.GET);
+
+            var response = http.Execute(request);
+            if (response.IsSuccessful)
+            {
+
+                var dto = JsonConvert.DeserializeObject<ResponseDTO>(response.Content);
+                return Ok(response.Content);
+            }
+
+            return BadRequest(response.ErrorMessage);
+        }
+
+    }
+}
---
+++ PDV.Webapi/ResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDV.Webapi
+{
+    public class ResponseDTO
+    {
+        public object Collections { get; set; }
+        public string Message { get; set; }
+        public bool HasError { get; set; }
+    }
+}
